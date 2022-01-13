425a93af2bc3ed849165d53fa57da8bcb1fc7d82
Prostopavl0 <97544991+Prostopavl0@users.noreply.github.com>
2022-01-13 14:26:13 +0200

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+import time
 
 link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/coders-at-work_207/"
 
@@ -8,3 +9,4 @@ def test_find_button(browser):
     browser.get(link)
     button = browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
     assert button.text == "Add to basket"
+    time.sleep(30)
